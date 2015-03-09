14d4d23e387769c114d7d40214ab154f199c4f27
void-sector <voidsector@foo.bar>
2015-03-09 10:54:14 +0100

    added jquery for bootstrap

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,6 @@
     <title>Laravel test project</title>
     <meta charset="utf-8">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>  
 </head>
 <body>
     
@@ -18,6 +17,9 @@
         </footer>
     
     </div>
+
+    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>  
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
     
 </body>
 </html>
